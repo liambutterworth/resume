603735c5026afb0004df5fc4d8dba8a6f77cc2ed
Liam Butterworth <account@liambutterworth.me>
2018-10-10 17:32:15 -0500

    add markup and styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
-const CleanWebpackPlugin   = require( 'clean-webpack-plugin' );
-const HtmlWebpackPlugin    = require( 'html-webpack-plugin' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const CleanWebpackPlugin      = require( 'clean-webpack-plugin' );
+const HtmlWebpackPlugin       = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin    = require( 'mini-css-extract-plugin' );
+const OptimizeCssAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
+const UglifyJsPlugin          = require( 'uglifyjs-webpack-plugin' );
 
 module.exports = {
+	plugins: [
+		new CleanWebpackPlugin( [ 'dist' ], { watch: true } ),
+		new HtmlWebpackPlugin( { template: 'src/index.html' } ),
+		new MiniCssExtractPlugin( { filename: '[name].[hash].css' } ),
+	],
+
+	optimization: {
+		minimizer: [
+			new OptimizeCssAssetsPlugin( {} ),
+			new UglifyJsPlugin( { cache: true, parallel: true } ),
+		],
+	},
+
 	output: {
 		filename: '[name].[hash].js',
 	},
 
-	plugins: [
-		new CleanWebpackPlugin( [ 'dist' ] ),
-		new HtmlWebpackPlugin( { template: 'src/index.html' } ),
-		new MiniCssExtractPlugin( { filename: '[name].[hash].css' } ),
-	]
-	
 	module: {
 		rules: [{
 			test:    /\.js$/,
